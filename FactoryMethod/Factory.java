e73e31dd45e87f659f5a47083cbec003ea0d4afa
legolas007 <oukuanou@163.com>
2018-04-06 22:47:56 +0800

    工厂方法模式

--- FactoryMethod/Factory.java
+++ FactoryMethod/Factory.java
@@ -5,7 +5,7 @@ import main.java.com.usher.DesignPatterns.SimpleFactory.Product;
 /**
  * 定义一个创建对象的接口，但是由子类决定要实例化那个类，
  * 工厂方法把实例化推迟到子类
- *
+ * 实例：java.util.Collection 接口的 iterator 方法，hibernate里通过sessionFactory创建session
  * 在简单工厂中，创建对象的是另一个类，而在工厂方法中，是由子类来创建对象
  */
 public abstract class Factory {
